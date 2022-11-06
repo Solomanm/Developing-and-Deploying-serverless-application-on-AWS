2d28b0cb28c1ef8a65feb1fa0ecb22817e9c8a6b
Solomanm <mtileni.proud@gmail.com>
2022-11-06 13:35:08 +0200

    serverless application

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'zca5ofdxmi';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-q0ojiaxy.us.auth0.com',
+  clientId: 'PcU3hZ2xjfTfdfRBzlObHJtI264LgcNZ',
+  callbackUrl: 'http://localhost:3000/callback'
+};
